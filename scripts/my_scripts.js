feedffdb986d51026facdaab09b2079fd3bae59b
Unknown <mortenolsen4@gmail.com>
2018-04-26 21:13:58 +0200

    Lagde litt jquery funksjon og en liste over ferdighter

---
+++ scripts/my_scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#ferdighter").hide();
+$("#opne").click(function(){
+$("#ferdighter").show();
+$("#opne").hide();
+
+});
+$("#lukke").click(function(){
+    $("#ferdighter").hide();
+    $("#opne").show();
+    
+    });
+});
